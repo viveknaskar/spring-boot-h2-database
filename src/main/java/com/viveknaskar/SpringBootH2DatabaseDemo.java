d3a4b152cb870fc74320c4d19488f1172f6f8d19
Vivek Naskar <viveknaskar2912@gmail.com>
2020-10-03 02:20:27 +0530

    H2 database integration with spring-boot app

---
+++ src/main/java/com/viveknaskar/SpringBootH2DatabaseDemo.java
@@ -0,0 +1,14 @@
+package com.viveknaskar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootH2DatabaseDemo {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringBootH2DatabaseDemo.class, args);
+
+    }
+}
